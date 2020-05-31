ba27c8190aff60ff7b71031534886ded97574bb2
LeonardIsOne <32675659+LeonardisOne@users.noreply.github.com>
2020-05-31 17:45:26 +0800

    add image and buton to app

--- App.js
+++ App.js
@@ -1,20 +1,56 @@
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import {
+  StyleSheet,
+  Text,
+  View,
+  TouchableHighlight,
+  Image,
+  SafeAreaView,
+  Button,
+} from "react-native";
 
 export default function App() {
-  let x = 1;
-  console.log("App Executed");
+  const handlePress = () => console.log("Text pressed");
   return (
-    <View style={styles.container}>
-      <Text>Hello to React Native!</Text>
-    </View>
+    // use SafeAreaView instead of View to deal with notch
+    <SafeAreaView style={styles.container}>
+      <Text onPress={handlePress}>Hello to React Native!</Text>
+      {/* require() bundle the image together with app, increase build size*/}
+      {/* <Image source={require("./assets/icon.png")} /> */}
+      {/* no feedback to user when touched */}
+      {/* <TouchableWithoutFeedback onPress={() => console.log("Image tapped")}>
+        <Image
+          source={{
+            width: 200, //require dimension for web images
+            height: 300,
+            uri: "https://picsum.photos/200/300",
+          }}
+        />
+      </TouchableWithoutFeedback> */}
+      <TouchableHighlight onPress={() => console.log("Image tapped")}>
+        <Image
+          source={{
+            width: 200, //require dimension for web images
+            height: 300,
+            uri: "https://picsum.photos/200/300",
+          }}
+        />
+      </TouchableHighlight>
+      {/* on iOS is a text button change text color, but Android is a rectangle button so color change background color*/}
+      <Button
+        color="orange"
+        title="Click me!"
+        onPress={() => console.log("button pressed")}
+      />
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
+    backgroundColor: "dodgerblue",
+    // needed for text to be at center, or notch can block the text
     alignItems: "center",
     justifyContent: "center",
   },
